ee691b878713872cdbd2fb548e855d5ed7db2151
Gabe Montague <montaguegabe@users.noreply.github.com>
2025-05-25 18:27:43 -0400

    Fix config

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+const s3Name = "gertrude";
+const useCdnBase = process.env.VITE_USE_CDN === "true";
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  base: useCdnBase ? `https://cdn.bighelp.ai/${s3Name}/` : "/",
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
